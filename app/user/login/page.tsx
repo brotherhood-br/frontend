1143f565e31390b173b568bba81517cd29a304de
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-06-09 23:59:51 -0300

    Add registration page

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -6,7 +6,6 @@ import { Command } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
-import { UserAuthForm } from "@/app/signin/components/user-auth-form"
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -17,15 +16,6 @@ export default function SignInPage() {
   return (
     <>
       <div className="container grid h-full place-items-center">
-        {/* <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost", size: "sm" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link> */}
         <div>
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2">
@@ -38,7 +28,7 @@ export default function SignInPage() {
               </p>
             </div>
             <Button variant="outline" type="button">
-              <Icons.google className="mr-2 h-4 w-4" /> Google
+              <Icons.google className="mr-2 h-4 w-4" /> Entrar com o Google
             </Button>
             <footer className="mt-60">
               <p className="text-sm text-muted-foreground">
---
+++ app/user/registration/page.tsx
@@ -0,0 +1,133 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const registrationFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      message: "Nome deve ter no mínimo 3 caracteres",
+    })
+    .max(255, {
+      message: "Nome deve ter no máximo 255 caracteres",
+    }),
+  birthDate: z
+    .string()
+    .min(10, {
+      message:
+        "Data de nascimento deve ter 10 caracteres no formato dd/mm/aaaa",
+    })
+    .max(10, {
+      message:
+        "Data de nascimento deve ter 10 caracteres no formato dd/mm/aaaa",
+    }),
+  email: z.string().email({ message: "Email inválido" }),
+  phone: z
+    .string()
+    .min(3, {
+      message: "Nome deve ter no mínimo 3 caracteres",
+    })
+    .max(255, {
+      message: "Nome deve ter no máximo 255 caracteres",
+    }),
+})
+
+type RegistrationFormValues = z.input<typeof registrationFormSchema>
+
+export default function RegistrationPage() {
+  const form = useForm<RegistrationFormValues>({
+    defaultValues: {
+      name: "",
+      birthDate: "",
+      email: "",
+      phone: "",
+    },
+    resolver: zodResolver(registrationFormSchema),
+  })
+
+  const onSubmit = (values: RegistrationFormValues) => {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="birthDate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Data de Nascimento</FormLabel>
+              <FormControl>
+                {/* TODO: use datepicker to select birthdate */}
+                {/* <DatePicker {...field} /> */}
+                <Input placeholder="dd/mm/aaaa" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Telefone</FormLabel>
+              <FormControl>
+                {/* TODO: use a library to handle patterns */}
+                {/* https://s-yadav.github.io/react-number-format/docs/pattern_format */}
+                <Input placeholder="( ) _____-____" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="vanderli@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="mt-auto w-full" type="submit">
+          Enviar
+        </Button>
+      </form>
+    </Form>
+  )
+}
