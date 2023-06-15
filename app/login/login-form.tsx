b8af88226a3c522cbdc43d34958847d8d8f27f9b
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-06-15 20:03:23 -0300

    Add loading to the login page

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -1,12 +1,14 @@
 "use client"
 
+import { useState } from "react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
-import { GoogleLogin } from "@react-oauth/google"
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google"
 
 import { api } from "@/lib/api"
 import { useAuth } from "@/hooks/useAuth"
 import { toast } from "@/components/ui/use-toast"
+import { Icons } from "@/components/icons"
 
 import { HomeResponse } from "../page"
 
@@ -15,8 +17,57 @@ export interface SignInFormProps {
 }
 
 export default function SignInForm({ inviteId }: SignInFormProps) {
+  const [isLoading, setIsLoading] = useState(false)
   const { login, setExternalToken } = useAuth()
   const router = useRouter()
+
+  const handleLoginSuccess = async (response: CredentialResponse) => {
+    if (!response.credential) throw new Error("No credential found")
+
+    setIsLoading(true)
+    const userData: HomeResponse = await api
+      .url("/home")
+      .headers({ sso_token: response.credential })
+      .get()
+      .forbidden(() => {
+        if (inviteId) {
+          // User is a member not registered
+          setExternalToken(response.credential!)
+          router.push(`/usuario/registrar/${inviteId}`)
+        } else {
+          // User is an admin not registered
+          setExternalToken(response.credential!)
+          router.push("/usuario/registrar")
+        }
+      })
+      .fetchError(() => {
+        toast({
+          title: "Erro",
+          description: "Não foi possível fazer login",
+          variant: "destructive",
+        })
+      })
+      .json()
+
+    if (!userData) {
+      setIsLoading(false)
+      return
+    }
+
+    // User is already registered
+    login({
+      user: {
+        id: userData.userId,
+        name: userData.userName,
+        role: userData.userType,
+        avatar: userData.brotherhoodLogo,
+      },
+      token: response.credential,
+    })
+
+    router.push("/")
+    setIsLoading(false)
+  }
 
   return (
     <div>
@@ -30,53 +81,18 @@ export default function SignInForm({ inviteId }: SignInFormProps) {
         </div>
 
         <div className="grid w-full place-items-center">
-          <GoogleLogin
-            onSuccess={async (response) => {
-              if (!response.credential) throw new Error("No credential found")
-
-              const userData: HomeResponse = await api
-                .url("/home")
-                .headers({ sso_token: response.credential })
-                .get()
-                .forbidden(() => {
-                  if (inviteId) {
-                    // User is a member not registered
-                    setExternalToken(response.credential!)
-                    router.push(`/usuario/registrar/${inviteId}`)
-                  } else {
-                    // User is an admin not registered
-                    setExternalToken(response.credential!)
-                    router.push("/usuario/registrar")
-                  }
-                })
-                .fetchError(() => {
-                  toast({
-                    title: "Erro",
-                    description: "Não foi possível fazer login",
-                    variant: "destructive",
-                  })
-                })
-                .json()
-
-              if (!userData) return
-
-              // User is already registered
-              login({
-                user: {
-                  id: userData.userId,
-                  name: userData.userName,
-                  role: userData.userType,
-                  avatar: userData.brotherhoodLogo,
-                },
-                token: response.credential,
-              })
-
-              router.push("/")
-            }}
-            onError={() => {
-              console.error("Login Failed")
-            }}
-          />
+          {isLoading ? (
+            <div>
+              <Icons.spinner className="h-6 w-6 animate-spin" />
+            </div>
+          ) : (
+            <GoogleLogin
+              onSuccess={handleLoginSuccess}
+              onError={() => {
+                console.error("Login Failed")
+              }}
+            />
+          )}
         </div>
         <footer className="mt-60">
           <p className="text-sm text-muted-foreground">
