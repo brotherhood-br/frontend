f8c45d13ad46f7f07ec467f0a69044a19a7566d0
Lucas Coelho <lucasfcoelho00@gmail.com>
2023-06-16 12:00:44 -0300

    Added drop-shadow to header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,7 +6,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full bg-background">
+    <header className="sticky top-0 z-40 w-full rounded-b-lg bg-background drop-shadow-2xl">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
       </div>
