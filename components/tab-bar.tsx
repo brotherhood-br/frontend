b69f11767b1ac0e9acafe9dc71cb0962d2ce4c1f
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-06-16 01:45:03 -0300

    Fix issues related to bottom menu

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -1,38 +1,37 @@
-import React from "react";
-import { useRouter } from "next/navigation"
-import { usePathname } from "next/navigation";
-import { Icons } from "@/components/icons";
+import React from "react"
+import { usePathname, useRouter } from "next/navigation"
 
+import { Icons } from "@/components/icons"
+
+const paths = {
+  brotherhood: "/republica",
+  finance: "/financas",
+  task: "/tarefas",
+  home: "/",
+}
 
 export function TabBar() {
+  const router = useRouter()
+  const pathname = usePathname()
 
-  const router = useRouter();
-  const pathname = usePathname();
+  const currentPath = Object.entries(paths).find(([, path]) =>
+    pathname.startsWith(path)
+  )?.[0]
 
-  let isHome = false;
-  let isBrotherhood = false;
-  let isFinance = false;
-  let isTask = false;
-
-  if(pathname.endsWith("/republica"))
-  isBrotherhood = true;
-  else if(pathname.endsWith("/financas"))
-  isFinance = true;
-  else if(pathname.endsWith("/tarefas"))
-  isTask = true;
-  else
-  isHome = true;
+  const isHome = currentPath === "home"
+  const isBrotherhood = currentPath === "brotherhood"
+  const isFinance = currentPath === "finance"
+  const isTask = currentPath === "task"
 
   return (
-    <div className="sticky inset-x-0 bottom-0 mb-0 flex h-16 w-screen items-center justify-between rounded-t-lg border-t border-gray-200 bg-background px-4 shadow-2xl">
+    <div className="sticky inset-x-0 bottom-0 flex h-[4.2rem] w-screen items-center justify-between rounded-t-lg border-t border-gray-200 bg-background px-4 pb-2 shadow-2xl">
       <button
         className={`flex flex-col items-center justify-center text-gray-500 hover:text-accent ${
           isHome ? "text-accent" : ""
         }`}
-        onClick={() => router.push("")}
+        onClick={() => router.push("/")}
       >
-        <Icons.home className="mb-1 h-6 w-6">
-          </Icons.home>
+        <Icons.home className="mb-1 h-6 w-6" />
         <span className="text-xs">Home</span>
       </button>
       <button
@@ -41,8 +40,7 @@ export function TabBar() {
         }`}
         onClick={() => router.push("/republica")}
       >
-        <Icons.users className="mb-1 h-6 w-6">
-        </Icons.users>
+        <Icons.users className="mb-1 h-6 w-6" />
         <span className="text-xs">República</span>
       </button>
       <button
@@ -51,8 +49,7 @@ export function TabBar() {
         }`}
         onClick={() => router.push("/financas")}
       >
-        <Icons.creditCard className="mb-1 h-6 w-6">
-        </Icons.creditCard>
+        <Icons.creditCard className="mb-1 h-6 w-6" />
         <span className="text-xs">Finanças</span>
       </button>
       <button
@@ -61,8 +58,7 @@ export function TabBar() {
         }`}
         onClick={() => router.push("/tarefas")}
       >
-        <Icons.clipBoard className="mb-1 h-6 w-6">
-        </Icons.clipBoard>
+        <Icons.clipBoard className="mb-1 h-6 w-6" />
         <span className="text-xs">Tarefas</span>
       </button>
     </div>
